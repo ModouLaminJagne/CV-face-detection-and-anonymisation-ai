35237ec1807260a01663a2288bf893e2e172dd22
Modou-Lamin-Jagne <jagnemlgh@gmail.com>
2025-09-06 02:47:06 +0000

    Implement code to process and blure images.

--- main.py
+++ main.py
@@ -3,50 +3,60 @@ import os
 import cv2
 import numpy as np
 import mediapipe as mp
+import argparse
 import os
 
+# function to process image
+def process_img(img, face_detection):
 
-output_dir = "./output"
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+    H, W, _ = img.shape
 
-
-# Read Image
-img_path = "C:\\Users\\Administrator\\Documents\\My Work\\CV-face-detection-and-anonymisation-ai\\IMG_4296.jpg"
-img = cv2.imread(img_path)
-
-H, W, _ = img.shape
-
-# Detect Image
-mp_face_detection = mp.solutions.face_detection
-with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.6) as face_detection:
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     out = face_detection.process(img_rgb)
+
     # print(out.detections)
     if out.detections is not None:
+
         for detection in out.detections:
+
             location_data = detection.location_data
             bboxC = location_data.relative_bounding_box
 
         x1, y1, w, h = bboxC.xmin, bboxC.ymin, bboxC.width, bboxC.height
         x1, y1, w, h = int(x1 * W), int(y1 * H), int(w * W), int((h * H)+20)
 
-        cv2.rectangle(img, (x1, y1), (x1 + w, y1 + h), (0, 255, 0), 20)
+        img[y1:y1 + h, x1:x1 + w, :] = cv2.blur(img[y1:y1 + h, x1:x1 + w, :], (30, 30))
+        
+    return img
+    
+args = argparse.ArgumentParser()
 
-        # Blur Image
-        Bimg = img.copy()
-        Bimg[y1:y1 + h, x1:x1 + w, :] = cv2.blur(Bimg[y1:y1 + h, x1:x1 + w, :], (70, 70))
-        # Bimg[y1:y1 + (h-5), x1:x1 + w, :] = cv2.GaussianBlur(Bimg[y1:y1 + (h-5), x1:x1 + w, :], (50, 50), 30)
-    else:
-        Bimg = img.copy()
-        print("No face detected")
+# Input mode: image, video, webcam
+args.add_argument("--mode", default='webcam')
+args.add_argument("--filePath", default=None)
 
-    # cv2.imshow("Image", Bimg)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+args = args.parse_args()
+
+# Create output directory
+output_dir = "./output"
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 
 
-# Save Image
-# cv2.imwrite("C:\\Users\\Administrator\\Desktop\\111_blurred2.jpg", Bimg)
-cv2.imwrite(os.path.join(output_dir, "blurred_image2.jpg"), Bimg)
-print("Image saved to:", os.path.join(output_dir, "blurred_image2.jpg"))
+# Detect Image
+mp_face_detection = mp.solutions.face_detection
+
+# Initialize Face Detection
+with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.6) as face_detection:
+
+    # Image Mode
+    if args.mode in ['image']:
+
+        # Read Image
+        img = cv2.imread(args.filePath)
+        img = process_img(img, face_detection)
+        
+        cv2.imwrite(os.path.join(output_dir, "blurred_pic.jpg"), img)
+        print("Image saved to:", os.path.join(output_dir, "blurred_pic.jpg"))
+
+    
